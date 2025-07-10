0b3de02369cd1945c7f4f9e1b221aff2b8bbfcd2
@ruachofeliyahu <ishaqdewolf@gmail.com>
2025-07-09 17:34:50 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mandelbrot_engine",
+    version="1.0.0",
+    description="Recursive Mandelbrot Sealer for mission authentication",
+    author="Isaac El Yshwaty",
+    packages=find_packages(),
+    install_requires=[],
+    python_requires=">=3.7",
+)
